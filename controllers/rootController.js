867b20fa1c883629540bba75de20cab67e8d1d94
Siddharth Kolipaka <kolipakasiddhu@gmail.com>
2025-02-16 05:13:54 +0530

    sus

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -1,5 +1,5 @@
 const handleRoot = (req, res) => {
-  return res.json({ message: "message from get req on /" });
+  return res.json({ message: "message from get req on domain/" });
 };
 
 export default handleRoot;
